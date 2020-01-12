82aaec415118a19b8d9f4ceeb3df7db4fd63dc92
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-11 22:30:15 -0500

    handling errors and installed morgan for logging the requests

--- node/node-rest-api/app.js
+++ node/node-rest-api/app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
 
+app.use(morgan("dev"));
 app.use("/orders", orderRoutes);
 app.use("/products", productRoutes);
 
-module.exports = app;
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 400;
+  next(error);
+});
 
+app.use((req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message
+    }
+  });
+});
+
+module.exports = app;
