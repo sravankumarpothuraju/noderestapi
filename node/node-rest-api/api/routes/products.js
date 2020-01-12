be24571c37d4545a5450f53976dc730cfc819ab3
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2020-01-11 23:20:28 -0500

    parsing  the body using bodyparser

--- node/node-rest-api/api/routes/products.js
+++ node/node-rest-api/api/routes/products.js
@@ -2,15 +2,20 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/products", (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.status(200).json({
     message: "Handling get requests to /products"
   });
 });
 
-router.post("/products", (req, res, next) => {
+router.post("/", (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  };
   res.status(201).json({
-    message: "Handling get requests to /products"
+    message: "Handling post requests to /products",
+    createdProduct: product
   });
 });
 
